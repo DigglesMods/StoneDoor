0c38a6df918073e778096087f46be2cd4d191614
Christian Voß <cech12@gmail.com>
2019-03-04 21:52:35 +0100

    add polish translation (thanks to shrike82)

--- Data/Scripts/text/doc/pl/tt_Steintuer.tcl
+++ Data/Scripts/text/doc/pl/tt_Steintuer.tcl
@@ -6,9 +6,9 @@ textwin print "/(is10)/(ildata/gui/icons/Steintuer.tga)"
 // ---- text starts here - do not change anything above this line ---
 
 ohlp_ttheadlinestyle
-textwin print "Stone Door"
+textwin print "Kamienne drzwi"
 ohlp_tttextbodystyle
-textwin print "The stone door, more stable than the wooden door, but has the same characteristics as the wooden door."
+textwin print "Kamienne drzwi s¹ bardziej stabilne ni¿ drewniane drzwi, jednak sprawdzaj¹ siê si samo jak one."
 
 // ---- do not change anything below this line ---
 
